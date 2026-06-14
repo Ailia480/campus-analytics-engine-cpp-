da7202d31d105522a96fd216956e6c671d5fe7dc
Ailia480 <f2025376480@umt.edu.pk>
2026-06-14 17:01:59 +0500

    Implement appendTXT, findRow, and rowExists

--- filehandler.cpp
+++ filehandler.cpp
@@ -83,3 +83,33 @@ void writeTXT(string filename, vector<string> header, vector<vector<string>> row
 
     file.close();
 }
+
+void appendTXT(string filename, vector<string> row) {
+    ofstream file(filename, ios::app);
+
+    for (int i = 0; i < row.size(); i++) {
+        file << formatField(row[i]);
+        if (i != row.size() - 1) file << ",";
+    }
+    file << endl;
+
+    file.close();
+}
+
+vector<string> findRow(string filename, int colIndex, string value) {
+    vector<vector<string>> data = readTXT(filename);
+
+    for (int i = 0; i < data.size(); i++) {
+        if (colIndex < data[i].size() && data[i][colIndex] == value) {
+            return data[i];
+        }
+    }
+
+    vector<string> empty;
+    return empty;
+}
+
+bool rowExists(string filename, int colIndex, string value) {
+    vector<string> result = findRow(filename, colIndex, value);
+    return !result.empty();
+}
